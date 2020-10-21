5bd83f2b13856ab4369c18abd5c962a22284d96d
Ashley Morgan Anderson <amanderson@albinoloverats.net>
2020-10-21 09:27:09 +0100

    INTERLOK-3196 Update tests

--- src/test/java/com/adaptris/core/mail/O365MailConsumerTest.java
+++ src/test/java/com/adaptris/core/mail/O365MailConsumerTest.java
@@ -1,36 +1,64 @@
 package com.adaptris.core.mail;
 
 import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.FixedIntervalPoller;
 import com.adaptris.core.MultiPayloadMessageFactory;
+import com.adaptris.core.Poller;
+import com.adaptris.core.QuartzCronPoller;
 import com.adaptris.core.StandaloneConsumer;
 import com.adaptris.core.stubs.MockMessageListener;
 import com.adaptris.core.util.LifecycleHelper;
 import com.adaptris.interlok.junit.scaffolding.ExampleConsumerCase;
+import com.adaptris.util.TimeInterval;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InterruptedIOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class O365MailConsumerTest extends ExampleConsumerCase
 {
+  private static final String APPLICATION_ID = "47ea49b0-670a-47c1-9303-0b45ffb766ec";
+  private static final String TENANT_ID = "cbf4a38d-3117-48cd-b54b-861480ee93cd";
+  private static final String CLIENT_SECRET = "NGMyYjY0MTEtOTU0Ny00NTg0LWE3MzQtODg2ZDAzZGVmZmY1Cg==";
+  private static final String USERNAME = "user@example.com";
+
+  private static final Poller[] POLLERS =
+  {
+    new FixedIntervalPoller(new TimeInterval(60L, TimeUnit.SECONDS)),
+    new QuartzCronPoller("0 */5 * * * ?"),
+  };
+
   private O365MailConsumer consumer;
 
+  private boolean runTests = false;
+
   @Before
-  public void setUp() throws IOException
+  public void setUp()
   {
+    Properties properties = new Properties();
+    try
+    {
+      properties.load(new FileInputStream(this.getClass().getResource("o365.properties").getFile()));
+      runTests = true;
+    }
+    catch (Exception e)
+    {
+      // do nothing
+    }
+
     consumer = new O365MailConsumer();
 
-    Properties properties = new Properties();
-    properties.load(new FileInputStream(this.getClass().getResource("o365.properties").getFile()));
-
-    consumer.setApplicationId(properties.getProperty("APPLICATION_ID"));
-    consumer.setTenantId(properties.getProperty("TENANT_ID"));
-    consumer.setClientSecret(properties.getProperty("CLIENT_SECRET"));
-    consumer.setUsername(properties.getProperty("USERNAME"));
+    consumer.setApplicationId(properties.getProperty("APPLICATION_ID", APPLICATION_ID));
+    consumer.setTenantId(properties.getProperty("TENANT_ID", TENANT_ID));
+    consumer.setClientSecret(properties.getProperty("CLIENT_SECRET", CLIENT_SECRET));
+    consumer.setUsername(properties.getProperty("USERNAME", USERNAME));
 
     consumer.setMessageFactory(new MultiPayloadMessageFactory());
   }
@@ -38,6 +66,8 @@ public class O365MailConsumerTest extends ExampleConsumerCase
   @Test
   public void testConsumer() throws Exception
   {
+    Assume.assumeTrue(runTests);
+
     MockMessageListener mockMessageListener = new MockMessageListener(10);
     StandaloneConsumer standaloneConsumer = new StandaloneConsumer(consumer);
     standaloneConsumer.registerAdaptrisMessageListener(mockMessageListener);
@@ -67,6 +97,19 @@ public class O365MailConsumerTest extends ExampleConsumerCase
   @Override
   protected Object retrieveObjectForSampleConfig()
   {
-    return consumer;
+    return new StandaloneConsumer(consumer);
+  }
+
+  @Override
+  protected List<StandaloneConsumer> retrieveObjectsForSampleConfig()
+  {
+    List<StandaloneConsumer> result = new ArrayList();
+    for (Poller poller : POLLERS)
+    {
+      StandaloneConsumer standaloneConsumer = (StandaloneConsumer)retrieveObjectForSampleConfig();
+      ((O365MailConsumer)standaloneConsumer.getConsumer()).setPoller(poller);
+      result.add(standaloneConsumer);
+    }
+    return result;
   }
 }
--- src/test/java/com/adaptris/core/mail/O365MailProducerTest.java
+++ src/test/java/com/adaptris/core/mail/O365MailProducerTest.java
@@ -4,43 +4,63 @@ import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.AdaptrisMessageFactory;
 import com.adaptris.core.NullConnection;
 import com.adaptris.core.StandaloneProducer;
-import com.adaptris.interlok.junit.scaffolding.ExampleConsumerCase;
+import com.adaptris.interlok.junit.scaffolding.ExampleProducerCase;
 import com.adaptris.interlok.junit.scaffolding.services.ExampleServiceCase;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
-public class O365MailProducerTest extends ExampleConsumerCase
+public class O365MailProducerTest extends ExampleProducerCase
 {
+  private static final String APPLICATION_ID = "47ea49b0-670a-47c1-9303-0b45ffb766ec";
+  private static final String TENANT_ID = "cbf4a38d-3117-48cd-b54b-861480ee93cd";
+  private static final String CLIENT_SECRET = "NGMyYjY0MTEtOTU0Ny00NTg0LWE3MzQtODg2ZDAzZGVmZmY1Cg==";
+  private static final String USERNAME = "user@example.com";
+
   private static final String SUBJECT = "InterlokMail Office365 Test Message";
   private static final String MESSAGE = "Bacon ipsum dolor amet tail landjaeger ribeye sausage, prosciutto pork belly strip steak pork loin pork bacon biltong ham hock leberkas boudin chicken. Brisket sirloin ground round, drumstick cupim rump chislic tongue short loin pastrami bresaola pork belly alcatra spare ribs buffalo. Swine chuck frankfurter pancetta. Corned beef spare ribs pork kielbasa, chuck jerky t-bone ground round burgdoggen.";
 
   private O365MailProducer producer;
 
+  private boolean runTests = false;
+
   @Before
-  public void setUp() throws IOException
+  public void setUp()
   {
+    Properties properties = new Properties();
+    try
+    {
+      properties.load(new FileInputStream(this.getClass().getResource("o365.properties").getFile()));
+      runTests = true;
+    }
+    catch (Exception e)
+    {
+      // do nothing
+    }
+
     producer = new O365MailProducer();
 
-    Properties properties = new Properties();
-    properties.load(new FileInputStream(this.getClass().getResource("o365.properties").getFile()));
-
-    producer.setApplicationId(properties.getProperty("APPLICATION_ID"));
-    producer.setTenantId(properties.getProperty("TENANT_ID"));
-    producer.setClientSecret(properties.getProperty("CLIENT_SECRET"));
-    producer.setUsername(properties.getProperty("USERNAME"));
+    producer.setApplicationId(properties.getProperty("APPLICATION_ID", APPLICATION_ID));
+    producer.setTenantId(properties.getProperty("TENANT_ID", TENANT_ID));
+    producer.setClientSecret(properties.getProperty("CLIENT_SECRET", CLIENT_SECRET));
+    producer.setUsername(properties.getProperty("USERNAME", USERNAME));
 
     producer.setSubject(SUBJECT);
-    producer.setToRecipients(properties.getProperty("USERNAME")); // send it to ourself so we're not spamming anyone else
+    producer.setToRecipients(properties.getProperty("USERNAME", USERNAME)); // send it to ourself so we're not spamming anyone else
     producer.setSave(true);
   }
 
   @Test
   public void testProducer() throws Exception
   {
+    Assume.assumeTrue(runTests);
+
     AdaptrisMessage message = AdaptrisMessageFactory.getDefaultInstance().newMessage(MESSAGE);
     StandaloneProducer standaloneProducer = new StandaloneProducer(new NullConnection(), producer);
     ExampleServiceCase.execute(standaloneProducer, message);
@@ -49,6 +69,12 @@ public class O365MailProducerTest extends ExampleConsumerCase
   @Override
   protected Object retrieveObjectForSampleConfig()
   {
-    return producer;
+    return new StandaloneProducer(new NullConnection(), producer);
+  }
+
+  @Override
+  protected List<StandaloneProducer> retrieveObjectsForSampleConfig()
+  {
+    return Collections.singletonList((StandaloneProducer)retrieveObjectForSampleConfig());
   }
 }
