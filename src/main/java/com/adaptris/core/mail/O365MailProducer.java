400c869c9df554876f03e86369907cf2b5e25ee6
Ashley Morgan Anderson <amanderson@albinoloverats.net>
2020-11-04 09:27:40 +0000

    INTERLOK-3196 Attachment support
    Handle sending emails with attachments if the Adaptris message is
    multi-payload capable.

--- src/main/java/com/adaptris/core/mail/O365MailProducer.java
+++ src/main/java/com/adaptris/core/mail/O365MailProducer.java
@@ -15,23 +15,33 @@ import com.microsoft.aad.msal4j.ClientCredentialFactory;
 import com.microsoft.aad.msal4j.ClientCredentialParameters;
 import com.microsoft.aad.msal4j.ConfidentialClientApplication;
 import com.microsoft.aad.msal4j.IAuthenticationResult;
+import com.microsoft.graph.concurrency.ChunkedUploadProvider;
+import com.microsoft.graph.concurrency.IProgressCallback;
+import com.microsoft.graph.core.ClientException;
 import com.microsoft.graph.models.extensions.Attachment;
+import com.microsoft.graph.models.extensions.AttachmentItem;
 import com.microsoft.graph.models.extensions.EmailAddress;
 import com.microsoft.graph.models.extensions.FileAttachment;
 import com.microsoft.graph.models.extensions.IGraphServiceClient;
 import com.microsoft.graph.models.extensions.ItemBody;
 import com.microsoft.graph.models.extensions.Message;
 import com.microsoft.graph.models.extensions.Recipient;
+import com.microsoft.graph.models.extensions.UploadSession;
+import com.microsoft.graph.models.generated.AttachmentType;
 import com.microsoft.graph.models.generated.BodyType;
 import com.microsoft.graph.requests.extensions.AttachmentCollectionPage;
+import com.microsoft.graph.requests.extensions.AttachmentCollectionRequest;
 import com.microsoft.graph.requests.extensions.GraphServiceClient;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.validation.constraints.NotBlank;
+import java.io.InputStream;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -149,32 +159,79 @@ public class O365MailProducer extends ProduceOnlyProducerImp
 
       if (adaptrisMessage instanceof MultiPayloadAdaptrisMessage)
       {
+        outlookMessage.isDraft = true;
+        Message draftMessage = graphClient.users(user).messages().buildRequest().post(outlookMessage);
+
         MultiPayloadAdaptrisMessage multiPayloadAdaptrisMessage = (MultiPayloadAdaptrisMessage)adaptrisMessage;
         String id = multiPayloadAdaptrisMessage.getCurrentPayloadId();
-        List<Attachment> attachments = new LinkedList<>();
-
-        // TODO find out how to actually attach stuff...
-        //outlookMessage.attachments = new AttachmentCollectionPage();
-
         for (String name : multiPayloadAdaptrisMessage.getPayloadIDs())
         {
           if (name.equals(id))
           {
             continue;
           }
-          FileAttachment attachment = new FileAttachment();
-          attachment.name = name;
-          attachment.contentType = "application/octet-stream";//"text/plain";
-          attachment.contentBytes = multiPayloadAdaptrisMessage.getPayload(name);
-          attachments.add(attachment);
-          if (outlookMessage.attachments != null)
+          long size = multiPayloadAdaptrisMessage.getSize(name);
+          /*
+           * Depending on the size of the file, you can choose one of
+           * two ways to attach a file to a message:
+           *  - If the file size is under 4 MB, you can do a single POST
+           *    on the attachments navigation property of the message.
+           *    The successful POST response includes the ID of the file
+           *    attached to the message.
+           *  - If the file size is between 3MB and 150MB, create an
+           *    upload session, and iteratively use PUT to upload ranges
+           *    of bytes of the file until you have uploaded the entire
+           *    file. A header in the final successful PUT response
+           *    includes a URL with the attachment ID.
+           */
+          if (size < 4 * FileUtils.ONE_MB)
           {
-            outlookMessage.attachments.getCurrentPage().add(attachment);
+            FileAttachment attachment = new FileAttachment();
+            attachment.oDataType = "#microsoft.graph.fileAttachment";
+            attachment.name = name;
+            attachment.size = (int)size;
+            attachment.contentType = "application/octet-stream";
+            attachment.contentBytes = Base64.getEncoder().encode(multiPayloadAdaptrisMessage.getPayload(name));
+            graphClient.users(user).messages(draftMessage.id).attachments().buildRequest().post(attachment);
+            graphClient.users(user).messages(draftMessage.id).send().buildRequest().post();
+          }
+          else
+          {
+            AttachmentItem attachment = new AttachmentItem();
+            attachment.attachmentType = AttachmentType.FILE;
+            attachment.name = name;
+            attachment.size = size;
+            attachment.contentType = "application/octet-stream";
+
+            UploadSession uploadSession = graphClient.users(user).messages(draftMessage.id).attachments().createUploadSession(attachment).buildRequest().post();
+            ChunkedUploadProvider<AttachmentItem> chunkedUploadProvider = new ChunkedUploadProvider<>(uploadSession, graphClient, multiPayloadAdaptrisMessage.getInputStream(name), attachment.size, AttachmentItem.class);
+            chunkedUploadProvider.upload(new IProgressCallback()
+            {
+              @Override
+              public void success(Object o)
+              {
+                graphClient.users(user).messages(draftMessage.id).send().buildRequest().post();
+              }
+
+              @Override
+              public void failure(ClientException ex)
+              {
+                log.error("Could not process attachment {} for message {} : {}", attachment.name, adaptrisMessage.getUniqueId(), ex);
+              }
+
+              @Override
+              public void progress(long current, long max)
+              {
+                // do nothing
+              }
+            });
           }
         }
       }
-
-      graphClient.users(user).sendMail(outlookMessage, save()).buildRequest().post();
+      else
+      {
+        graphClient.users(user).sendMail(outlookMessage, save()).buildRequest().post();
+      }
     }
     catch (Exception e)
     {
